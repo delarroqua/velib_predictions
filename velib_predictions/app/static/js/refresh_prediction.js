206bf2eab153a96403966b3797762966f8900478
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2016-12-24 18:43:09 +0100

    predict result with javascript instead of html conditions

---
+++ velib_predictions/app/static/js/refresh_prediction.js
@@ -0,0 +1,25 @@
+$(function(){
+    $("#prediction-result").hide()
+})
+
+$('#prediction-form').submit(function(e){
+    e.preventDefault();
+    $.post(
+        'prediction',
+        {
+            number_station: $('#number_station').val(),
+        },
+        function(data){
+            prediction = data['prediction']
+            $("#prediction").text(prediction)
+            $("#prediction-result").show()
+
+//            // Hide evaluation box if errors box is not empty
+//            if ( $('#errors-box').css("display") == 'block' ) {
+//                $('#prediction-result').hide();
+//            } else {
+//                $("#prediction-result").show()
+//            }
+        }
+    );
+});
--- velib_predictions/app/views.py
+++ velib_predictions/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from velib_predictions.app.predict import *
 from velib_predictions.app import app
 from velib_predictions.utils.io import load_pickle
@@ -22,17 +22,24 @@ model = load_pickle("files/model.pkl")
 # list_stations = pd.read_csv('list_stations.csv')
 
 # request example : curl -i http://localhost:5000/prediction/4006
-@app.route('/prediction', methods=['GET'])
+@app.route('/prediction', methods=['POST'])
 def ask_prediction():
-    number_station = request.args.get('number_station')
-    if (number_station.isdigit()):
-        number_station = int(number_station)
-        prediction = predict_available_bikes(model, number_station)
-    else:
-        prediction = -1
-    return render_template('prediction.html', number_station=number_station, prediction=prediction) # list_stations=list_stations
+    number_station = request.form['number_station']
+    prediction = predict_available_bikes(model, number_station)
+    return jsonify({'prediction': prediction})
 
 
 @app.route('/')
 def index():
     return render_template('prediction.html') # list_stations=list_stations
+
+
+# @app.route('/prediction', methods=['GET'])
+# def ask_prediction_old():
+#     # number_station = request.args.get('number_station')
+#     if (number_station.isdigit()):
+#         number_station = int(number_station)
+#         prediction = predict_available_bikes(model, number_station)
+#     else:
+#         prediction = -1
+#     return render_template('prediction.html', number_station=number_station, prediction=prediction) # list_stations=list_stations
