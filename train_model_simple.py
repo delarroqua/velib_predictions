4c76193387ef7f05790e2fa73e128377a76fa58c
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2017-01-04 14:42:18 +0100

    define and move get_features_and_targets to data_loader.py, and test train_model_simple

--- train_model_simple.py
+++ train_model_simple.py
@@ -1,17 +1,14 @@
 from velib_modules.connection.db_connection import PostgresConnection
+from velib_modules.connection.data_loader import get_features_and_targets
 
 from velib_modules.model.model import RFTransformer
 from velib_modules.model.evaluation import evaluate_model
 from velib_modules.model.info import compute_model_information
 
-from velib_modules.utils.io import load_json, paths_exist, export_pickle, load_pickle, load_dataframe_pickle, export_dataframe_pickle
-from velib_modules.utils.df import SplitFeaturesTarget, FilterPostalCode
-from velib_modules.utils.station_enricher import enrich_stations_simple
-
-from sklearn.model_selection import train_test_split
+from velib_modules.utils.io import load_json, paths_exist, export_pickle, load_pickle
 
 import os
-import time
+
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,60 +18,28 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
 
     # Set out_directory
-    out_directory = "files/app_model/"
+    out_directory = "files/simple_model/"
 
     # Set features of model
     columns_model_list = ['number', 'weekday', 'hour', 'minute', 'latitude', 'longitude', 'available_bikes_previous',
                           'weekday_previous', 'hour_previous', 'minute_previous']
 
+    postal_code_list = ['75004', '75011']
+    target_column = "available_bikes"
+
+    config_query = {
+        "columns": "number, address, latitude, longitude, available_bikes, last_update, last_update_previous, available_bikes_previous",
+        "table": "other.update_stations_with_previous_variables",
+        "limit": 10000
+    }
+
+    # Create Connection
+    config_db = load_json("config/config_db.json")
+    connection = PostgresConnection(config_db)
+
     # Load data
-    if paths_exist(os.path.join(out_directory,"features_train.pkl"), os.path.join(out_directory,"features_test.pkl"),
-                   os.path.join(out_directory,"target_train.pkl"), os.path.join(out_directory,"target_test.pkl")):
-        logger.info("Retrieving features train and test from cache")
-        features_train = load_dataframe_pickle(os.path.join(out_directory,"features_train.pkl"))
-        features_test = load_dataframe_pickle(os.path.join(out_directory,"features_test.pkl"))
-        target_train = load_dataframe_pickle(os.path.join(out_directory,"target_train.pkl"))
-        target_test = load_dataframe_pickle(os.path.join(out_directory,"target_test.pkl"))
-    else:
-        postal_code_list = ['75004', '75011']
-        target_column = "available_bikes"
-
-        # Create Connection
-        config_db = load_json("config/config_db.json")
-        connection = PostgresConnection(config_db)
-
-        query = """
-                select number, address, latitude, longitude, available_bikes, last_update, last_update_previous, available_bikes_previous
-                from {{table}}
-                limit {{limit}}
-                """
-        config_query = {"table": "other.update_stations_with_previous_variables", "limit": 50000}
-        stations_raw_df = connection.query(query, config_query)
-
-        # Filter df
-        stations_filtered_df = FilterPostalCode(stations_raw_df, postal_code_list)
-
-        # Enrich station
-        logger.info("Enrich dataframe")
-        start = time.time()
-        df_enriched = enrich_stations_simple(stations_filtered_df)
-        enricher_running_time = time.time() - start
-        logger.info("Running enricher took %s", enricher_running_time)
-
-        logger.info("Ratio data_enriched/data_raw : %s", len(df_enriched)/len(stations_raw_df))
-
-        # Get features and target, divided by train & test
-        logger.info("Split target and features")
-        features, target = SplitFeaturesTarget(df_enriched, target_column)
-        logger.info("Train/test split")
-        features_train, features_test, target_train, target_test = \
-            train_test_split(features, target, test_size=0.2, random_state=42)
-
-        logger.info("Exporting splitted dataset...")
-        export_dataframe_pickle(features_train, os.path.join(out_directory,"features_train.pkl"))
-        export_dataframe_pickle(features_test, os.path.join(out_directory,"features_test.pkl"))
-        export_dataframe_pickle(target_train, os.path.join(out_directory,"target_train.pkl"))
-        export_dataframe_pickle(target_test, os.path.join(out_directory,"target_test.pkl"))
+    features_train, features_test, target_train, target_test = \
+        get_features_and_targets(target_column, postal_code_list, connection, config_query, out_directory)
 
     # Load model
     if paths_exist(os.path.join(out_directory,"model.pkl")):
--- velib_modules/connection/data_loader.py
+++ velib_modules/connection/data_loader.py
@@ -1,11 +1,59 @@
-from ..utils.io import paths_exist, export_dataframe_pickle, load_dataframe_pickle
+from velib_modules.utils.df import SplitFeaturesTarget, FilterPostalCode
+from velib_modules.utils.station_enricher import enrich_stations_simple
 
+from velib_modules.utils.io import paths_exist, export_dataframe_pickle, load_dataframe_pickle
+
+from sklearn.model_selection import train_test_split
+
+import os
+import time
 import pandas as pd
 
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def get_features_and_targets(target_column, postal_code_list, connection, config_query, out_directory):
+    # Load data
+    if paths_exist(os.path.join(out_directory,"features_train.pkl"), os.path.join(out_directory,"features_test.pkl"),
+                   os.path.join(out_directory,"target_train.pkl"), os.path.join(out_directory,"target_test.pkl")):
+        logger.info("Retrieving features train and test from cache")
+        features_train = load_dataframe_pickle(os.path.join(out_directory,"features_train.pkl"))
+        features_test = load_dataframe_pickle(os.path.join(out_directory,"features_test.pkl"))
+        target_train = load_dataframe_pickle(os.path.join(out_directory,"target_train.pkl"))
+        target_test = load_dataframe_pickle(os.path.join(out_directory,"target_test.pkl"))
+    else:
+        query = """ select {{columns}} from {{table}} limit {{limit}} """
+        stations_raw_df = connection.query(query, config_query)
+
+        # Filter df
+        stations_filtered_df = FilterPostalCode(stations_raw_df, postal_code_list)
+
+        # Enrich station
+        logger.info("Enrich dataframe")
+        start = time.time()
+        df_enriched = enrich_stations_simple(stations_filtered_df)
+        enricher_running_time = time.time() - start
+        logger.info("Running enricher took %s", enricher_running_time)
+
+        logger.info("Ratio data_enriched/data_raw : %s", len(df_enriched)/len(stations_raw_df))
+
+        # Get features and target, divided by train & test
+        logger.info("Split target and features")
+        features, target = SplitFeaturesTarget(df_enriched, target_column)
+        logger.info("Train/test split")
+        features_train, features_test, target_train, target_test = \
+            train_test_split(features, target, test_size=0.2, random_state=42)
+
+        logger.info("Exporting splitted dataset...")
+        export_dataframe_pickle(features_train, os.path.join(out_directory,"features_train.pkl"))
+        export_dataframe_pickle(features_test, os.path.join(out_directory,"features_test.pkl"))
+        export_dataframe_pickle(target_train, os.path.join(out_directory,"target_train.pkl"))
+        export_dataframe_pickle(target_test, os.path.join(out_directory,"target_test.pkl"))
+    return features_train, features_test, target_train, target_test
+
 
 def load_weather_data(path_weather_data):
     weather_data_raw = pd.read_csv(path_weather_data)
