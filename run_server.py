78501e06b222bf4689b71b26b9a0129e64f18b96
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2017-01-31 22:22:08 +0100

    add necessary files to build heroku app

--- run_server.py
+++ run_server.py
@@ -3,5 +3,7 @@
 from velib_modules.app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
