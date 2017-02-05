cd7dbf9b3f39aa2c2b16bd8d61d0149e93ba727d
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2017-02-05 20:12:14 +0100

    change port from local to port 80

--- run_server.py
+++ run_server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from velib_modules.app import app
+import os
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    #port = int(os.environ.get("PORT", 80))
+    #app.run(host='0.0.0.0', port=port)
 
