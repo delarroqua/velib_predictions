5a484948b8e697777be5adfea27710df1dc6f680
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2017-02-12 20:57:43 +0100

    add metrics in evaluate_model and clean metrics.py

--- velib_modules/model/evaluation.py
+++ velib_modules/model/evaluation.py
@@ -1,8 +1,10 @@
-from velib_modules.model.metrics import root_mean_squared_error, within_two
+from velib_modules.model.metrics import root_mean_squared_error, within_two, mean_percentage_error, mean_absolute_error
 
 def evaluate_model(model, features_test, y_test):
     y_pred = model.predict(features_test)
     performance = {}
     performance['RMSE'] = root_mean_squared_error(y_test, y_pred)
+    performance['MAPE'] = mean_percentage_error(y_test, y_pred)
+    performance['MAE'] = mean_absolute_error(y_test, y_pred)
     performance['within_two'] = within_two(y_test, y_pred)
     return performance
--- velib_modules/model/metrics.py
+++ velib_modules/model/metrics.py
@@ -1,27 +1,14 @@
 import numpy as np
 
 
-def within_error(y_true, y_pred, error=0.1):
-    return np.mean(np.abs(y_true - y_pred) / (y_true + 1e-6) <= error)
-
-
-def within_error_xgb(preds, dtrain, error=0.1):
-    labels = dtrain.get_label()
-    return 'WITHIN_0.1', within_error(labels, preds, error)
-
-
 def within_two(y_true, y_pred, threshold=2):
     return np.mean(np.abs(y_true - y_pred) <= threshold)
 
-
-def within_two_xgb(preds, dtrain, error=2):
-    labels = dtrain.get_label()
-    return 'WITHIN_2', within_two(labels, preds, error)
-
+def mean_percentage_error(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred) / (y_true + 1e-6))
 
 def root_mean_squared_error(y_true, y_pred):
     return ((y_true - y_pred) ** 2).mean() ** 0.5
-
 
 def mean_absolute_error(y_true, y_pred):
     return np.abs(y_true - y_pred).mean()
