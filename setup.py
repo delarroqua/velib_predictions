6d738ab06a5d1494ed833d2983d1d18ad182d233
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2017-02-12 16:43:11 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='velib-prediction',
       description='Vélib - availability prediction',
       version='0.1',
       install_requires=['scipy', 'numpy', 'pandas==0.19.1', 'scikit-learn==0.18.1', 'db.py==0.5.2',
-                        'psycopg2', 'flask==0.11.1'],
+                        'psycopg2', 'flask==0.11.1', 'Jinja2==2.8', 'requests==2.9.1'],
       packages=find_packages(),
      )
